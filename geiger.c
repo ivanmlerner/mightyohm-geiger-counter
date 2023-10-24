089d25e8d7d6d29723e6801392dc2d8625e147b3
Ivan Marquesi Lerner <ivanmlerner@protonmail.com>
2023-10-24 11:58:03 -0300

    Adjust comments

--- geiger.c
+++ geiger.c
@@ -1,70 +1,69 @@
 /*
   Title: Geiger Counter with Serial Data Reporting
   Description: This is the firmware for the mightyohm.com Geiger Counter.
-  There is more information at http://mightyohm.com/geiger 
-
+  There is more information at http://mightyohm.com/geiger
+  
   Author:         Jeff Keyzer
   Company:        MightyOhm Engineering
   Website:        http://mightyohm.com/
   Contact:        jeff <at> mightyohm.com
-
+  
   This firmware controls the ATtiny2313 AVR microcontroller on board the Geiger Counter kit.
-
+  
   When an impulse from the GM tube is detected, the firmware flashes the LED and produces a short
   beep on the piezo speaker.  It also outputs an active-high pulse (default 100us) on the PULSE pin.
-
+  
   A pushbutton on the PCB can be used to mute the beep.
-
+  
   A running average of the detected counts per second (CPS), counts per minute (CPM), and equivalent dose
-  (uSv/hr) is output on the serial port once per second. The dose is based on information collected from 
+  (uSv/hr) is output on the serial port once per second. The dose is based on information collected from
   the web, and may not be accurate.
-
+  
   The serial port is configured for BAUD baud, 8-N-1 (default 9600).
-
+  
   The data is reported in comma separated value (CSV) format:
   CPS, #####, CPM, #####, uSv/hr, ###.##, SLOW|FAST|INST
-
+  
   There are three modes.  Normally, the sample period is LONG_PERIOD (default 60 seconds). This is SLOW averaging mode.
   If the last five measured counts exceed a preset threshold, the sample period switches to SHORT_PERIOD seconds (default 5 seconds).
   This is FAST mode, and is more responsive but less accurate. Finally, if CPS > 255, we report CPS*60 and switch to
   INST mode, since we can't store data in the (8-bit) sample buffer.  This behavior could be customized to suit a particular
   logging application.
-
+  
   The largest CPS value that can be displayed is 65535, but the largest value that can be stored in the sample buffer is 255.
-
+  
   ***** WARNING *****
   This Geiger Counter kit is for EDUCATIONAL PURPOSES ONLY.  Don't even think about using it to monitor radiation in
   life-threatening situations, or in any environment where you may expose yourself to dangerous levels of radiation.
   Don't rely on the collected data to be an accurate measure of radiation exposure! Be safe!
-
-
+  
   Change log:
   08/04/11 1.00: Initial release for Chaos Camp 2011!
   24/10/23 1.10: Add average measure and long press to reset.
-
+  
   Copyright 2011 Jeff Keyzer, MightyOhm Engineering
-
+  
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
+  
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
+  
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // Includes
-#include <avr/io.h>             // this contains the AVR IO port definitions
-#include <avr/interrupt.h>      // interrupt service routines
-#include <avr/pgmspace.h>       // tools used to store variables in program memory
-#include <avr/sleep.h>          // sleep mode utilities
-#include <util/delay.h>         // some convenient delay functions
-#include <stdlib.h>             // some handy functions like utoa()
+#include <avr/io.h>                     // this contains the AVR IO port definitions
+#include <avr/interrupt.h>              // interrupt service routines
+#include <avr/pgmspace.h>               // tools used to store variables in program memory
+#include <avr/sleep.h>                  // sleep mode utilities
+#include <util/delay.h>                 // some convenient delay functions
+#include <stdlib.h>                     // some handy functions like utoa()
 
 // Defines
 #define VERSION                 "1.10"
@@ -116,16 +115,15 @@ uint8_t mode;                           // logging mode, 0 = slow, 1 = fast, 2 =
 */
 ISR(INT0_vect) {
   if (count < UINT16_MAX) // check for overflow, if we do overflow just cap the counts at max possible
-    count++; // increase event counter
+    count++;              // increase event counter
   
   // send a pulse to the PULSE connector
   // a delay of 100us limits the max CPS to about 8000
   // you can comment out this code and increase the max CPS possible (up to 65535!)
-  PORTD |= _BV(PD6);      // set PULSE output high
+  PORTD |= _BV(PD6);            // set PULSE output high
   _delay_us(PULSEWIDTH);
-  PORTD &= ~(_BV(PD6));   // set pulse output low
-  
-  eventflag = 1;  // tell main program loop that a GM pulse has occurred
+  PORTD &= ~(_BV(PD6));         // set pulse output low
+  eventflag = 1;                // tell main program loop that a GM pulse has occurred
 }
 
 /*
@@ -135,10 +133,9 @@ ISR(INT0_vect) {
   execute multiple times if we're not careful.
 */
 ISR(INT1_vect) {
-  _delay_ms(25);                  // slow down interrupt calls (crude debounce)
-  
-  if ((PIND & _BV(PD3)) == 0)     // is button still pressed?
-    nobeep ^= 1;         // toggle mute mode
+  _delay_ms(25);                 // slow down interrupt calls (crude debounce)
+  if ((PIND & _BV(PD3)) == 0)    // is button still pressed?
+    nobeep ^= 1;                 // toggle mute mode
   
   _delay_ms(975);
   if ((PIND & _BV(PD3)) == 0) {
@@ -150,25 +147,25 @@ ISR(INT1_vect) {
   while ((PIND & _BV(PD3)) == 0)
     _delay_ms(100);
   
-  EIFR |= _BV(INTF1);             // clear interrupt flag to avoid executing ISR again due to switch bounce
+  EIFR |= _BV(INTF1);            // clear interrupt flag to avoid executing ISR again due to switch bounce
 }
 
 /*
-  Timer1 compare interrupt 
+  Timer1 compare interrupt
   This interrupt is called every time TCNT1 reaches OCR1A and is reset back to 0 (CTC mode).
   Timer1 is setup so this happens once a second.
 */
 ISR(TIMER1_COMPA_vect) {
-  uint8_t i;      // index for fast mode
-  tick = 1;       // update flag
+  uint8_t i;                     // index for fast mode
+  tick = 1;                      // update flag
   
-  //PORTB ^= _BV(PB4);            // toggle the LED (for debugging purposes)
+  //PORTB ^= _BV(PB4);           // toggle the LED (for debugging purposes)
   cps = count;
   sum_avg += cps;
   seconds++;
-  slowcpm -= buffer[idx];         // subtract oldest sample in sample buffer
+  slowcpm -= buffer[idx];        // subtract oldest sample in sample buffer
   
-  if (count > UINT8_MAX) {        // watch out for overflowing the sample buffer
+  if (count > UINT8_MAX) {       // watch out for overflowing the sample buffer
     count = UINT8_MAX;
     overflow = 1;
   }
@@ -178,19 +175,19 @@ ISR(TIMER1_COMPA_vect) {
   
   // Compute CPM based on the last SHORT_PERIOD samples
   fastcpm = 0;
-  for(i=0; i<SHORT_PERIOD;i++) {
+  for(i=0; i<SHORT_PERIOD;i++) {                   // sum up the last 5 CPS values
     int8_t x = idx - i;
     if (x < 0)
       x = LONG_PERIOD + x;
-    fastcpm += buffer[x];   // sum up the last 5 CPS values
+    fastcpm += buffer[x];
   }
-  fastcpm = fastcpm * (LONG_PERIOD/SHORT_PERIOD); // convert to CPM
+  fastcpm = fastcpm * (LONG_PERIOD/SHORT_PERIOD);  // convert to CPM
   
   // Move to the next entry in the sample buffer
   idx++;
   if (idx >= LONG_PERIOD)
     idx = 0;
-  count = 0;  // reset counter
+  count = 0;                                       // reset counter
 }
 
 // Functions
@@ -203,40 +200,33 @@ void uart_putchar(char c) {
 }
 
 // Send a string in SRAM to the UART
-void uart_putstring(char *buffer) {
-  // start sending characters over the serial port until we reach the end of the string
-  while (*buffer != '\0') {       // are we at the end of the string yet?
-    uart_putchar(*buffer);  // send the contents
-    buffer++;                               // advance to next char in buffer
+void uart_putstring(char *buffer) {       // start sending characters over the serial port until we reach the end of the string
+  while (*buffer != '\0') {               // are we at the end of the string yet?
+    uart_putchar(*buffer);                // send the contents
+    buffer++;                             // advance to next char in buffer
   }
 }
 
 // Send a string in PROGMEM to the UART
-void uart_putstring_P(char *buffer) {
-  // start sending characters over the serial port until we reach the end of the string
-  while (pgm_read_byte(buffer) != '\0')   // are we done yet?
+void uart_putstring_P(char *buffer) {       // start sending characters over the serial port until we reach the end of the string
+  while (pgm_read_byte(buffer) != '\0')     // are we done yet?
     uart_putchar(pgm_read_byte(buffer++));  // read byte from PROGMEM and send it
 }
 
 // flash LED and beep the piezo
 void checkevent(void) {
-  if (eventflag) {                // a GM event has occurred, do something about it!
-    eventflag = 0;          // reset flag as soon as possible, in case another ISR is called while we're busy
-    
-    PORTB |= _BV(PB4);      // turn on the LED
-    
-    if(!nobeep) {           // check if we're in mute mode
-      TCCR0A |= _BV(COM0A0);  // enable OCR0A output on pin PB2
-      TCCR0B |= _BV(CS01);    // set prescaler to clk/8 (1Mhz) or 1us/count
-      OCR0A = 160;    // 160 = toggle OCR0A every 160ms, period = 320us, freq= 3.125kHz
+  if (eventflag) {              // a GM event has occurred, do something about it!
+    eventflag = 0;              // reset flag as soon as possible, in case another ISR is called while we're busy
+    PORTB |= _BV(PB4);          // turn on the LED
+    if(!nobeep) {               // check if we're in mute mode
+      TCCR0A |= _BV(COM0A0);    // enable OCR0A output on pin PB2
+      TCCR0B |= _BV(CS01);      // set prescaler to clk/8 (1Mhz) or 1us/count
+      OCR0A = 160;              // 160 = toggle OCR0A every 160ms, period = 320us, freq= 3.125kHz
     }
-    
-    // 10ms delay gives a nice short flash and 'click' on the piezo
-    _delay_ms(10);
-    PORTB &= ~(_BV(PB4));   // turn off the LED
-    
-    TCCR0B = 0;                             // disable Timer0 since we're no longer using it
-    TCCR0A &= ~(_BV(COM0A0));       // disconnect OCR0A from Timer0, this avoids occasional HVPS whine after beep
+    _delay_ms(10);              // 10ms delay gives a nice short flash and 'click' on the piezo
+    PORTB &= ~(_BV(PB4));       // turn off the LED
+    TCCR0B = 0;                 // disable Timer0 since we're no longer using it
+    TCCR0A &= ~(_BV(COM0A0));   // disconnect OCR0A from Timer0, this avoids occasional HVPS whine after beep
   }       
 }
 
@@ -245,7 +235,6 @@ void sendreport(void) {
   uint32_t cpm;   // This is the CPM value we will report
   if(tick) {      // 1 second has passed, time to report data via UART
     tick = 0;       // reset flag for the next interval
-    
     if (overflow) {
       cpm = cps*60UL;
       mode = 2;
@@ -281,7 +270,6 @@ void sendreport(void) {
     // this reports the integer part
     utoa((uint16_t)(usv_scaled/10000), serbuf, 10); 
     uart_putstring(serbuf);
-    
     uart_putchar('.');
     
     // this reports the fractional part (2 decimal places)
@@ -353,8 +341,7 @@ int main(void) {
   
   sei();  // Enable interrupts
   
-  while(1) {
-    // Configure AVR for sleep, this saves a couple mA when idle
+  while(1) {                          // Configure AVR for sleep, this saves a couple mA when idle
     set_sleep_mode(SLEEP_MODE_IDLE);  // CPU will go to sleep but peripherals keep running
     sleep_enable();                   // enable sleep
     sleep_cpu();                      // put the core to sleep
